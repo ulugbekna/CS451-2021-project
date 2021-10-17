8eaf3e6c1b81b01f153f7a0e7ffbb57b2a71ca55
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-17 11:22:52 +0200

    add Packet, AckPacket, and MessagePacket classes

---
+++ template_java/src/main/java/cs451/packets/AckPacket.java
@@ -0,0 +1,5 @@
+package cs451.packets;
+
+public class AckPacket extends Packet {
+    public AckPacket(int id) {super(id);}
+}
---
+++ template_java/src/main/java/cs451/packets/Packet.java
@@ -0,0 +1,8 @@
+package cs451.packets;
+
+public abstract class Packet {
+    public final int id;
+
+    protected Packet(int id) {this.id = id;}
+}
+
