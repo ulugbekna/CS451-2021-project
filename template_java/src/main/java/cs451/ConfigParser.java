3a7ca46563d9a631a7ebeb0c0761667a141b4ce8
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-17 11:23:24 +0200

    add parsing for perfect links config file

--- template_java/src/main/java/cs451/ConfigParser.java
+++ template_java/src/main/java/cs451/ConfigParser.java
@@ -1,6 +1,9 @@
 package cs451;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ConfigParser {
 
@@ -16,4 +19,31 @@ public class ConfigParser {
         return path;
     }
 
+    public PerfectLinksConfig[] parsePerfectLinks() throws IOException {
+        var configFilePath = Paths.get(path);
+        var lines = Files.lines(configFilePath);
+        var configStream = lines.map((s) -> {
+            var ns = s.split(" ");
+            assert ns.length == 2;
+            var nMessages = Integer.parseInt(ns[0]);
+            var hostIdx = Integer.parseInt(ns[1]);
+            return new PerfectLinksConfig(nMessages, hostIdx);
+        });
+        return configStream.toArray(PerfectLinksConfig[]::new);
+    }
+
+    protected static class PerfectLinksConfig {
+        public final int nMessages;
+        public final int hostIdx;
+
+        public PerfectLinksConfig(int nMessages, int hostIdx) {
+            this.nMessages = nMessages;
+            this.hostIdx = hostIdx;
+        }
+
+        @Override
+        public String toString() {
+            return "PerfectLinksConfig{" + "nMessages=" + nMessages + ", hostIdx=" + hostIdx + '}';
+        }
+    }
 }
