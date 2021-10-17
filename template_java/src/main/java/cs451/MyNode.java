af41dd4f743aed51373970d4fc627bb9dfd34665
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-17 11:24:27 +0200

    initial version of `MyNode`

---
+++ template_java/src/main/java/cs451/MyNode.java
@@ -0,0 +1,20 @@
+package cs451;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyNode {
+    public final Node me;
+    public final HashMap<Integer, Node> peers;
+    public AtomicInteger msgUid = new AtomicInteger(0);
+
+    public MyNode(Node me, HashMap<Integer, Node> peers) {
+        this.me = me;
+        this.peers = peers;
+    }
+
+    @Override
+    public String toString() {
+        return "MyNode{" + "me=" + me + ", peers=" + peers + ", msgUid=" + msgUid + '}';
+    }
+}
