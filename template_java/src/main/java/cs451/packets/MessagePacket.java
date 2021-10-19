76c03ea002e138440188f4b49ebea6d168fc52bc
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-19 20:58:54 +0200

    add `equals` and `hashCode` method implementations to Packet and `MessagePacket`

--- template_java/src/main/java/cs451/packets/MessagePacket.java
+++ template_java/src/main/java/cs451/packets/MessagePacket.java
@@ -1,6 +1,7 @@
 package cs451.packets;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class MessagePacket extends Packet implements Serializable {
     public final String message;
@@ -8,6 +9,20 @@ public class MessagePacket extends Packet implements Serializable {
     public MessagePacket(int senderId, int id, String message) {
         super(senderId, id);
         this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MessagePacket that = (MessagePacket) o;
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), message);
     }
 
     @Override
--- template_java/src/main/java/cs451/packets/Packet.java
+++ template_java/src/main/java/cs451/packets/Packet.java
@@ -1,6 +1,7 @@
 package cs451.packets;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public abstract class Packet implements Serializable {
     public final int senderId;
@@ -9,6 +10,19 @@ public abstract class Packet implements Serializable {
     protected Packet(int senderId, int id) {
         this.senderId = senderId;
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Packet packet = (Packet) o;
+        return senderId == packet.senderId && id == packet.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(senderId, id);
     }
 
     @Override
