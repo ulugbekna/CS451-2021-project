c105af611be0a6a5f83424fee2f62285ecd20e69
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-22 18:56:25 +0200

    remove unnecessary field from `MyNode`

--- template_java/src/main/java/cs451/MyNode.java
+++ template_java/src/main/java/cs451/MyNode.java
@@ -6,13 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MyNode {
     public final Node me;
     public final HashMap<Integer, Node> peers;
-    public final PeerMsgTbl<Boolean> seenMessages;
     public AtomicInteger msgUid = new AtomicInteger(1);
 
     public MyNode(Node me, HashMap<Integer, Node> peers) {
         this.me = me;
         this.peers = peers;
-        seenMessages = new PeerMsgTbl<>();
     }
 
     @Override
