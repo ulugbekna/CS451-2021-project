47f5012ece0a1da739923568887796e2e78cc6d5
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-10-24 19:04:52 +0200

    make `MyNode.msgId` non atomic

--- template_java/src/main/java/cs451/MyNode.java
+++ template_java/src/main/java/cs451/MyNode.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MyNode {
     public final Node me;
     public final HashMap<Integer, Node> peers;
-    public AtomicInteger msgUid = new AtomicInteger(1);
+    public int msgUid = 1;
 
     public MyNode(Node me, HashMap<Integer, Node> peers) {
         this.me = me;
