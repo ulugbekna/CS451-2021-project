2bea79166c92223c3ed202030d5c4187f364a368
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-12-14 14:00:22 +0100

    enable tracing

--- template_java/src/main/java/cs451/Main.java
+++ template_java/src/main/java/cs451/Main.java
@@ -70,7 +70,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         /* START: Program state initialization */
-        Log.NONE();
+        Log.TRACE();
 
         initSignalHandlers();
 
