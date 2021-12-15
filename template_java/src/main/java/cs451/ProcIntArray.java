c36058a5a7384f5986c90bbe8e94a5a9dba190a5
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-12-15 12:53:05 +0100

    implement ProcIntArray

---
+++ template_java/src/main/java/cs451/ProcIntArray.java
@@ -0,0 +1,28 @@
+package cs451;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ProcIntArray implements Iterable<Integer> {
+    private final int[] arr;
+
+    public ProcIntArray(int nProcs) {
+        arr = new int[nProcs];
+    }
+
+    /**
+     * careful: can return null if the value isn't set
+     */
+    public int getById(int id) {
+        return arr[id - 1];
+    }
+
+    public void setById(int id, int v) {
+        arr[id - 1] = v;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return Arrays.stream(arr).iterator();
+    }
+}
